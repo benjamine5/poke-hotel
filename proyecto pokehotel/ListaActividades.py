ba896202fe0f5750ffb36e629e94721f48b59041
benjasmines <143446989+benjamine5@users.noreply.github.com>
2024-04-20 23:29:12 -0400

    Add files via upload

---
+++ proyecto pokehotel/ListaActividades.py
@@ -0,0 +1,19 @@
+class Actividades:
+    def __init__(self, nadar, paseo, fogata, sJuegos):
+        self.nadar = nadar
+        self.paseo = paseo
+        self.fogata = fogata
+        self.sJuegos = sJuegos
+
+
+    def get_nadar(self):
+        return self.nadar
+
+    def get_paseo(self):
+        return self.paseo
+
+    def get_fogata(self):
+        return self.fogata
+
+    def get_sJuegos(self):
+        return self.sJuegos
---
+++ proyecto pokehotel/interfas.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+raiz = Tk()
+raiz.title("Hotel pokemon")
+raiz.resizable(True,True)
+raiz.iconbitmap("pokemon.ico")
+raiz.config(bg="white")
+raiz.geometry("650x350")
+raiz.mainloop ()
