ecdf98fde2dd802e152906ff727f27f7f8cf6d3c
NineFox596 <143448301+NineFox596@users.noreply.github.com>
2024-05-06 23:05:12 -0400

    InterfasActividades.py

--- proyecto pokehotel/interfasActividades.py
+++ proyecto pokehotel/interfasActividades.py
@@ -1,26 +1,50 @@
-#importamos las librerias
 from tkinter import *
 
-#creamos la ventana
+#Funcion recopiladora de datos de los checkbuttons (odie esta parte -n-)
+def compra():
+    if(paseo.get()==1):
+        print("Paseo seleccionado")
+    else:
+        print("Paseo sin Seleccionar")
+    if(nadar.get()==1):
+        print("Nadar Seleccionado")
+    else:
+        print("Nadar sin Seleccionar")
+    if(fogata.get()==1):
+        print("Fogata Seleccionado")
+    else:
+        print("Fogata sin Seleccionar")
+    if(juegos.get()==1):
+        print("Juegos Seleccionado")
+    else:
+        print("Juegos sin Seleccionar")
+    
+
+#creamos la ventana :D
 raiz=Tk()
 raiz.geometry("800x600")
 raiz.title("Actividades")
 raiz.config(bd=10, relief="groove")
 raiz.iconbitmap("pokemon.ico")
 
-#texto actividad
+#texto actividad ;)
 frameActividad=Frame(raiz)
 frameActividad.pack(side="top", fill="x")
 frameActividad.config(bd=10, relief="groove")
 Label(frameActividad, text="Actividades", font=("Times New Romans", 35)).pack()
 
-#Lista de actividades
-Checkbutton(raiz, text="Paseo", font=("Times New Romans", 25)).place(x=150, y=125)
-Checkbutton(raiz, text="Nadar", font=("Times New Romans", 25)).place(x=150, y=200)
-Checkbutton(raiz, text="Fogata", font=("Times New Romans", 25)).place(x=150, y=275)
-Checkbutton(raiz, text="sJuegos", font=("Times New Romans", 25)).place(x=150, y=350)
+nadar=IntVar()
+fogata=IntVar()
+juegos=IntVar()
+paseo=IntVar()
 
-#boton siguiente
+#Lista de actividades D:
+Opcion1=Checkbutton(raiz, text="Paseo", font=("Times New Romans", 25), variable=paseo, onvalue=1, offvalue=0, command=compra).place(x=150, y=125)
+Opcion2=Checkbutton(raiz, text="Nadar", font=("Times New Romans", 25), variable=nadar, onvalue=1, offvalue=0, command=compra).place(x=150, y=200)
+Opcion3=Checkbutton(raiz, text="Fogata", font=("Times New Romans", 25), variable=fogata, onvalue=1, offvalue=0, command=compra).place(x=150, y=275)
+Opcion4=Checkbutton(raiz, text="Juegos", font=("Times New Romans", 25), variable=juegos, onvalue=1, offvalue=0, command=compra).place(x=150, y=350)
+
+#boton siguiente :>
 def codigoBoton():
     print("Boton presionado")
 botonEnvio=Button(raiz, text="Siguiente", font=("Times New Romans", 20), command=codigoBoton)
