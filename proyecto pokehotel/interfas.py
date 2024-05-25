ff66afde4af2e4d5031e6eb70366326f6f9585cf
benjasmines <143446989+benjamine5@users.noreply.github.com>
2024-05-25 16:13:33 -0400

    Update interfas.py

--- proyecto pokehotel/interfas.py
+++ proyecto pokehotel/interfas.py
@@ -1,38 +1,22 @@
 # lo necesario (crear un venv)
 import tkinter as tk
+import subprocess #la libreria para abrir lo demas
 from PIL import Image, ImageTk
 
 # creamos la pestaña
 raiz = tk.Tk()
-raiz.geometry('400x400')  # Set the initial size of the window
-raiz.iconbitmap("pokemon.ico")
+raiz.geometry('400x400')
 
-# imagen del ditto
-image = Image.open("ditto.png")
-image = image.resize((200, 200)) # Set the desired width and height of the image
+# imagen de fondo
+image = Image.open("image.png")
+image = image.resize((400, 400))
 photo_image = ImageTk.PhotoImage(image)
 image_label = tk.Label(raiz, image=photo_image)
 image_label.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
 
 # agregamos 2 botones
 def on_button_click1():
-    #crea la nueva ventana
-    new_window = tk.Tk()
-
-    # cambio del tamaño
-    new_window.geometry('600x500')
-
-    # hace las matrices
-    for i in range(3):
-        matrix = tk.Frame(new_window)
-        matrix.pack()
-
-        for j in range(6):
-            label = tk.Label(matrix, text=f"Matrix {i+1}, Cell {j+1}")
-            label.pack(side=tk.LEFT)
-
-    # el loop de la nueva ventana
-    new_window.mainloop()
+    subprocess.Popen(["python","seatSelection.py"])
 
 def on_button_click2():
     print("si lo clickearon ")
